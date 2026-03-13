1610f6dd65dbe02807fe3b31d6386e1747fa0baa
jmartingarciacuerva <jmartingarciacuerva@educa.madrid.org>
2026-03-13 11:41:11 +0100

    Segundo

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
+        // IntelliJ IDEA suggests fixing it.asdfasdfasdfa
         System.out.printf("Hello and welcome!");
 
         // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
